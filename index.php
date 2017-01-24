28f430c54a943861f5dcd888b72b2f39dfd91494
Maurice Achtenhagen <maurice@achtenhagen.me>
2017-01-24 18:10:41 -0500

    Add Sea Bird to portfolio

--- index.php
+++ index.php
@@ -7,8 +7,10 @@
 	<img src="images/projects/limella_v2.png" alt="Limella v2" />
 	<p><strong>Inferno</strong> is a simple Twitter client for iOS devices with a heavy emphasis on minimalism. It aims to take away some of the noise that holds Twitter back from having a great user experience. Using artificial intelligence, Inferno takes your Twitter timeline and transforms it into a simple news stream, showing you only what’s relevant. The idea behind this project is to see how minimal an app could be, while still providing a great UX. It will be to be released later this year.</p>
 	<img src="images/projects/inferno.png" alt="Inferno" />
-	<p><strong>Releasify</strong> was my first iOS application and has been live on the App Store since January 2016. The idea behind this project was to go against all odds and create an application that is designed to never be opened. This unique user experience challenge proved to be successful, despite the general idea of creating experiences that draw in the user’s attention as much as possible. More on this project: <a href="https://releasify.me" target="_blank">https://releasify.me</a>.</p>
+	<p><a href="https://releasify.me" target="_blank">Releasify</a> was my first iOS application and has been live on the App Store since January 2016. The idea behind this project was to go against all odds and create an application that is designed to never be opened. This unique user experience challenge proved to be successful, despite the general idea of creating experiences that draw in the user’s attention as much as possible.</p>
 	<img src="images/projects/releasify.png" alt="Releasify" />
+	<p><a href="https://github.com/achtenhagen/SeaBird" target="_blank">Sea Bird</a> was my first mobile application. It was live on the webOS App Gallery in 2011 and quickly became the most downloaded application in its category. Sea Bird supported RSS and Atom news feeds and came bundled with an RSS repository, which was hosted on my own server. This allowed users to search for and rank content.</p>
+	<img src="images/projects/seabird.png" alt="Sea Bird" />
 </section>
 <section id="desc">
 	<p>Read more about me <a href="about.php">here</a>.</p>
